3f3f08f74c7378b12644d4425a8c0fa40936053f
asrofie <asrofie@gmail.com>
2021-09-05 20:14:28 +0700

    remove unused import

--- lib/bloc/property_cubit.dart
+++ lib/bloc/property_cubit.dart
@@ -1,13 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tbs_app/api/api_service.dart';
 import 'package:tbs_app/bloc/property_state.dart';
-import 'package:tbs_app/model/property_model.dart';
 import 'package:tbs_app/model/user_model.dart';
-import 'package:tbs_app/validation.dart' as validator;
-import 'package:tbs_app/bloc/login_state.dart';
 import 'package:tbs_app/bloc/app_state.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
 
 class PropertyCubit extends Cubit<AppState> {
   int attempt = 0;
